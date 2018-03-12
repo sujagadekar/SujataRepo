8e55e0383e1597fdcdc26ab98c554f09a873e068
Sujata Gadekar <suja.gadekar@gmail.com>
2018-03-12 16:44:18 +0530

    ok

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,21 @@
+
+class Circle{
+		int radius;
+	
+		Circle(int r){
+			radius = r;			
+		}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Circle obj = new Circle(5);
+		System.out.println(obj.radius);
+		Object o = new Object();
+		
+		
+	}
+
+}
